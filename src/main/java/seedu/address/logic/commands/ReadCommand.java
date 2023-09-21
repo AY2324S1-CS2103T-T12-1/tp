b0377de42985ac4e3383571ca9ee7105b3c789b7
VanessaMae23 <vanessamae23@gmail.com>
2023-09-21 13:46:40 +0800

    Fix

--- src/main/java/seedu/address/logic/commands/ReadCommand.java
+++ src/main/java/seedu/address/logic/commands/ReadCommand.java
@@ -1,4 +1,9 @@
 package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import java.util.List;
+
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
@@ -6,10 +11,10 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
-import java.util.List;
 
-import static java.util.Objects.requireNonNull;
-
+/**
+ * Reads a person identified using it's displayed index from the address book.
+ */
 public class ReadCommand extends Command {
 
     public static final String COMMAND_WORD = "read";
