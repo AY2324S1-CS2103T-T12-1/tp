d0dcf4e4df069fa9f7a0673e75c32ba262748f76
leesiyuan <lee.si.yuan.20@gmail.com>
2023-10-13 15:32:59 +0800

    Update ClearCommandTest.java
    Added testing for emptyAddressBook failure.

--- src/test/java/seedu/address/logic/commands/ClearCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearCommandTest.java
@@ -29,4 +29,12 @@ public class ClearCommandTest {
         assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
     }
 
+    @Test
+    public void execute_emptyAddressBook_failure() {
+        Model model = new ModelManager();
+        Model expectedModel = new ModelManager();
+
+        assertCommandSuccess(new ClearCommand(), model, ClearCommand.MESSAGE_SUCCESS, expectedModel);
+    }
+
 }
